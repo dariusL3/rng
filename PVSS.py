a1df38b849809cb9bc5d01547acbfed8fb895a50
Dat <dat96le@gmail.com>
2017-10-01 10:30:58 +0700

    1/10: ZKP added

--- PVSS.py
+++ PVSS.py
@@ -3,6 +3,7 @@ import codecs
 from gmpy2 import mpz
 import time
 import sys
+from Crypto.Hash import SHA256
 
 offset = 120
 #uses Miller-Rabin primality test, which is always correct when it  yields False, but has a chance to incorrectly yield True.
@@ -22,12 +23,16 @@ def test(args):
 		return
 	ss = PVSS(p)
 	pairs = ss.generateTestKeyPairs(n)
-	shares = ss.PVSSDistribute(secret, n,t)
+	[shares,proof] = ss.PVSSDistribute(secret, n,t)
+	print "Proof: "+str(proof)
+	print ss.verifyZKP(shares,proof[0],proof[1],proof[2])
+	print ss.generatorSecondary
+	print pairs
 	dec = [ss.decryptShare(shares[i], pairs[i][0]) for i in range(n)]
 	param1 = [i+offset for i in range(t)]
 	param2 = [dec[i] for i in range(t)]
 	res = ss.PVSSReconstruct(param1, param2)
-	print res
+	print "Recovered secret: "+str(res)
 
 
 class PVSS:
@@ -73,14 +78,15 @@ class PVSS:
 			f.write(secret.digits(10)+"\n")
 		poly = [self.sample(self.prime) for i in range(t-1)]
 		poly.append(s)
-		print poly
-		result = []
+		preSecrets = []
+		shares = []
 		for i in range(n):
-			share = self.evaluatePoly(poly, i+offset, self.prime-1)
-			#sup.append(share)
-			share = gmpy2.powmod(publicKeys[i], share, self.prime)
-			result.append(share)
-		return result
+			temp = self.evaluatePoly(poly, i+offset, self.prime-1)
+			preSecrets.append(temp)
+			temp = gmpy2.powmod(publicKeys[i], temp, self.prime)
+			shares.append(temp)
+		proof = self.generateZKP(preSecrets, shares)
+		return [shares,proof]
 
 	def ShamirReconstruct(self, xl, yl):
 		n = len(xl)
@@ -97,7 +103,7 @@ class PVSS:
 			lambnum = gmpy2.f_mod(mpz(lambnum), self.prime)
 			lambden = gmpy2.f_mod(mpz(lambden), self.prime)
 			lamb = gmpy2.f_mod(lambnum*gmpy2.invert(lambden,self.prime),self.prime)
-			print (lambnum,lambden,lamb)
+			#print (lambnum,lambden,lamb)
 			result += lamb*yl[i]
 		return gmpy2.f_mod(result, self.prime)
 
@@ -119,7 +125,7 @@ class PVSS:
 			#lamb = gmpy2.f_mod(lambnum*gmpy2.invert(lambden,self.prime),self.prime)
 			lamb = lambnum / lambden
 			lamb = gmpy2.f_mod(lamb, self.prime-1)
-			print (lambnum,lambden,lamb)
+			#print (lambnum,lambden,lamb)
 			temp = gmpy2.powmod(dSecrets[i],lamb, self.prime)
 			result = gmpy2.f_mod(temp*result, self.prime)
 		return result
@@ -147,6 +153,45 @@ class PVSS:
 		inv = gmpy2.invert(secretKey,self.prime-1)
 		result = gmpy2.powmod(share,inv,self.prime)
 		return result
+
+	def generateZKP(self,preSecrets,shares):
+		vArr = []
+		wArr = []
+		n = len(preSecrets)
+
+		h = SHA256.new()
+		for i in range(n):
+			vArr.append(gmpy2.powmod(self.generatorSecondary, preSecrets[i], self.prime))
+			w = self.sample(self.prime-1)
+			wArr.append(w)
+			temp = (gmpy2.powmod(self.publicKeys[i],w,self.prime),gmpy2.powmod(self.generatorSecondary,w,self.prime))
+			temp = (temp[0].digits(10), temp[1].digits(10))
+			print temp
+			h.update(bytes(temp[0]))
+			h.update(bytes(temp[1]))
+		print wArr
+		e = mpz(h.hexdigest(),base=16)
+		e = gmpy2.f_mod(e,self.prime-1)
+		
+		zArr = [gmpy2.f_mod(wArr[i]-preSecrets[i]*e, self.prime-1) for i in range(n)]
+
+		return [vArr,e,zArr]
+
+	def verifyZKP(self,shares,vArr,e,zArr):
+		n = len(shares)
+		h = SHA256.new()
+		for i in range(n):
+			temp = (gmpy2.powmod(self.publicKeys[i],zArr[i],self.prime)*gmpy2.powmod(shares[i],e,self.prime),\
+				gmpy2.powmod(self.generatorSecondary,zArr[i],self.prime)*gmpy2.powmod(vArr[i],e,self.prime))
+			temp = (gmpy2.f_mod(temp[0],self.prime), gmpy2.f_mod(temp[1], self.prime))
+			temp = (temp[0].digits(10), temp[1].digits(10))
+			print temp
+			h.update(bytes(temp[0]))
+			h.update(bytes(temp[1]))
+		eVerify = mpz(h.hexdigest(),base=16)
+		eVerify = gmpy2.f_mod(eVerify,self.prime-1)
+		
+		return eVerify==e
 
 
 if __name__=="__main__":
