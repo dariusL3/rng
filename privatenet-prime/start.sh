e229153ec501fdec0b78c26058f67fcfe2213e8a
Le Tien Dat <dat96le@gmail.com>
2018-05-03 10:14:02 +0700

    Mistaken

--- privatenet-prime/start.sh
+++
@@ -1,9 +0,0 @@
-gnome-terminal -x sh -c "./eth-private-net start lily; bash"
-gnome-terminal -x sh -c "./eth-private-net start chipu; bash"
-gnome-terminal -x sh -c "./eth-private-net start hari; bash"
-gnome-terminal -x sh -c "./eth-private-net start htram; bash"
-gnome-terminal -x sh -c "./eth-private-net start mytam; bash"
-gnome-terminal -x sh -c "./eth-private-net start alice; bash"
-gnome-terminal -x sh -c "./eth-private-net start baoanh; bash"
-
-./connect.sh
