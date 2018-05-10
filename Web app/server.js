bb57deefc804d14202c88e23e0d7e58a3516e350
Dat LE <dat96le@gmail.com>
2018-05-10 16:17:30 +0700

    Change dir

--- Web app/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-app.use(express.static('public'))
-app.use(express.static('node_modules'))
-app.use(cors('http://localhost:8103'))
-
-app.listen(8080, () => console.log('Server listening on port 8080'))
