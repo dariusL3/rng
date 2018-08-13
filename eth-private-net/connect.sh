0ff72ef40ec3e3ccccbd0688c171c4a378e4ff58
Dat LE <datlt@blockchainlabs.asia>
2018-08-13 09:45:52 +0700

    prnet refresh

--- eth-private-net/connect.sh
+++
@@ -1,6 +0,0 @@
-./eth-private-net connect lily hari
-./eth-private-net connect lily htram
-./eth-private-net connect lily chipu
-./eth-private-net connect lily baoanh
-./eth-private-net connect lily mytam
-./eth-private-net connect lily alice
